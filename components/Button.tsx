fd371ad2c995a847d8ff3a1fe21e3259d4e7f9c8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-23 11:25:48 +0200

    dev: added primary btn

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,37 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { StyleSheet, View, Pressable, Text } from "react-native";
 
 interface ButtonProps {
   label: string;
+  theme?: string;
 }
 
-export default function Button({ label }: ButtonProps) {
+export default function Button({ label, theme }: ButtonProps) {
+  if (theme === "primary") {
+    return (
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 },
+        ]}
+      >
+        <Pressable
+          style={[styles.button, { backgroundColor: "#fff" }]}
+          onPress={() => alert("You pressed a button.")}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>
+            {label}
+          </Text>
+        </Pressable>
+      </View>
+    );
+  }
   return (
     <View style={styles.buttonContainer}>
       <Pressable
