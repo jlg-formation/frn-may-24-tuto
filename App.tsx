243f71b05ebdaa7b0138f5e69798dbe9db81678f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-23 11:11:45 +0200

    dev: added buttons

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
@@ -9,6 +10,10 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -24,5 +29,9 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
