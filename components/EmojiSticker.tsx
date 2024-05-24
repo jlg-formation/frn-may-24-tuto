b061a6194418d134e45c079160ff67c3347756f5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-24 10:51:42 +0200

    dev: pan

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -17,6 +17,8 @@ export default function EmojiSticker({
   stickerSource,
 }: EmojiStickerProps) {
   const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
 
   const doubleTap = Gesture.Tap()
     .numberOfTaps(2)
@@ -35,15 +37,35 @@ export default function EmojiSticker({
     };
   });
 
+  const drag = Gesture.Pan().onChange((event) => {
+    translateX.value += event.changeX;
+    translateY.value += event.changeY;
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
   return (
-    <View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-        <Animated.Image
-          source={stickerSource}
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]}
-        />
-      </GestureDetector>
-    </View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={stickerSource}
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+          />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
   );
 }
